df2f8efe396558a3eec07ad9c6f128a789855b91
Micki Harning <mickiharning@gmail.com>
2024-01-31 23:39:13 +0100

    changed config class

--- config.py
+++ config.py
@@ -1,13 +1,28 @@
+# import os
+# # The absolute path of the directory where the program resides.
+# # In this case: /Users/micki/Documents/Programmering/projects/Javascript/React/nlp-webapp/src/Api
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+# class Config(object):
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+#     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:NxWYoZaY78cydE9yW6Gt@containers-us-west-70.railway.app:6688/railway'
+#     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:NxWYoZaY78cydE9yW6Gt@containers-us-west-70.railway.app:6688/railway'
+#     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
 import os
-# The absolute path of the directory where the program resides.
-# In this case: /Users/micki/Documents/Programmering/projects/Javascript/React/nlp-webapp/src/Api
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:NxWYoZaY78cydE9yW6Gt@containers-us-west-70.railway.app:6688/railway'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:NxWYoZaY78cydE9yW6Gt@containers-us-west-70.railway.app:6688/railway'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    if not SECRET_KEY:
+        raise ValueError("No SECRET_KEY set for Flask application")
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    if 'DATABASE_URL' not in os.environ:
+        print("Warning: DATABASE_URL is not set, falling back to local database.")
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ConfigDBTest(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
